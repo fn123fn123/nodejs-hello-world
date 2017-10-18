4fa9f46dbe58d5bcda47280e8c53c0604012fbcf
Felipe Negron <fnegron@scholastic.com>
2017-10-18 14:42:49 -0400

    playing with static content

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-var http = require('http');
-//loads http module
-var app=http.createServer(function (req, res) {
-//creates server
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  //sets the right header and status code
-  res.end('Hello World\n');
-  //outputs string with line end symbol
-}).listen(8080);
-//sets port and IP address of the server
-console.log('Server running...');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
